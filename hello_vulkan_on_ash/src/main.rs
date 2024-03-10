93e79486c124701d51653322af16db3ffa4b8b2f
Shuto Shikama <shuto.shikama@gmail.com>
2024-03-10 12:29:44 +0900

    [WIP] Vulkan のサンプルを追加

---
+++ hello_vulkan_on_ash/src/main.rs
@@ -0,0 +1,47 @@
+use std::{borrow::Cow, ffi::CStr};
+
+use ash::vk;
+
+fn main() {
+    let entry = unsafe { ash::Entry::new() }.unwrap();
+    let app_info = vk::ApplicationInfo {
+        api_version: vk::make_api_version(0, 1, 0, 0),
+        ..Default::default()
+    };
+    let create_info = vk::InstanceCreateInfo {
+        p_application_info: &app_info,
+        ..Default::default()
+    };
+    let instance = unsafe { entry.create_instance(&create_info, None) }.unwrap();
+    let physical_devices = unsafe { instance.enumerate_physical_devices() }.unwrap();
+
+    println!("Hello, world!");
+}
+
+unsafe extern "system" fn vulkan_debug_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _user_data: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    let callback_data = *p_callback_data;
+    let message_id_number = callback_data.message_id_number;
+
+    let message_id_name = if callback_data.p_message_id_name.is_null() {
+        Cow::from("")
+    } else {
+        CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy()
+    };
+
+    let message = if callback_data.p_message.is_null() {
+        Cow::from("")
+    } else {
+        CStr::from_ptr(callback_data.p_message).to_string_lossy()
+    };
+
+    println!(
+        "{message_severity:?}:\n{message_type:?} [{message_id_name} ({message_id_number})] : {message}\n",
+    );
+
+    vk::FALSE
+}
